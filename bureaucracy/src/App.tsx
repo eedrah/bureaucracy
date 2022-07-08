6991f822ecaf4a4de27c8b2608cdaf3fa23afe5d
Eedrah <9520856+eedrah@users.noreply.github.com>
2022-07-09 06:45:00 +1200

    make the back office

--- bureaucracy/src/App.tsx
+++ bureaucracy/src/App.tsx
@@ -4,6 +4,7 @@ import { Offices } from './funcs/logic'
 
 import Office from './views/Office'
 import Page from './views/Page'
+import BackOffice from './views/BackOffice'
 
 const theme = responsiveFontSizes(createTheme())
 
@@ -13,7 +14,23 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Page />}>
-            <Route index element={<Office office={Offices.Reception} />} />
+            <Route index element={<BackOffice />} />
+            <Route
+              path={Offices[Offices.Reception].toLocaleLowerCase()}
+              element={<Office office={Offices.Reception} />}
+            />
+            <Route
+              path={Offices[Offices.HR].toLocaleLowerCase()}
+              element={<Office office={Offices.HR} />}
+            />
+            <Route
+              path={Offices[Offices.Stationary].toLocaleLowerCase()}
+              element={<Office office={Offices.Stationary} />}
+            />
+            <Route
+              path={Offices[Offices.IT].toLocaleLowerCase()}
+              element={<Office office={Offices.IT} />}
+            />
           </Route>
         </Routes>
       </HashRouter>
