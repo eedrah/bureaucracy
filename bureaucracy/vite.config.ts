0aecd905d1d18a30930a2be9307ef23bd502062b
Eedrah <9520856+eedrah@users.noreply.github.com>
2022-07-08 21:10:46 +1200

    add emotion css transform

--- bureaucracy/vite.config.ts
+++ bureaucracy/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
 })
